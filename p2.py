5ae19cfa1670c682dd0dca194a5825d9c8ba6c04
Prajwalais <prajwalbakaningi7@gmail.com>
2025-10-23 14:35:14 +0530

    CI

--- p2.py
+++ p2.py
@@ -4,3 +4,14 @@ T = float(input("Enter Time: "))
 
 SI = (P * R * T) / 100
 print(f"Simple Interest: ₹{SI}")
+
+
+P = float(input("Enter Principal: "))
+R = float(input("Enter Rate: "))
+T = float(input("Enter Time: "))
+
+SI = (P * R * T) / 100
+CI = P * ((1 + R / 100) ** T) - P
+
+print(f"Simple Interest: ₹{SI}")
+print(f"Compound Interest: ₹{CI:.2f}")
